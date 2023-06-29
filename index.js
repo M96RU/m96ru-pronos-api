acd614bab9d0b0b03b9c79e2fe80f75e1abc99ec
Manuel RUSSO <manuel.russo@worldline.com>
2023-06-29 21:31:00 +0200

    Retrieve matches

--- index.js
+++ index.js
@@ -4,28 +4,54 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const cache = require('map-expire');
+const fetch = require('node-fetch');
 
-const duration = 10 * 60 * 1000; // 10 minutes
+const matches = new Map();
+
+const url = 'https://odds.p.rapidapi.com/v4/sports/soccer_usa_mls/odds?regions=eu&oddsFormat=decimal&markets=h2h&dateFormat=iso';
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '14a06c0ec5msh472e321cca5703ep1f33fdjsn7c843c14d3a3',
+        'X-RapidAPI-Host': 'odds.p.rapidapi.com'
+    }
+};
+
+function retrieveMatches() {
+    try {
+        console.log('Retrieve matches...');
+        fetch(url, options)
+            .then(response => response.json())
+            .then(saveMatches)
+            .catch(err => console.error(err))
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function saveMatches(body) {
+    console.log('Retrieved matches: ' + body.length);
+    body.forEach(match => {
+        let matchId = '' + match.id;
+        matches.set(matchId, match);
+    });
+}
+
+var period = 60 * 60 * 1000; // X minutes
+retrieveMatches();
+setInterval(retrieveMatches, period);
 
 app.get('/', (req, res) => {
     res.send("Express on Vercel with CORS");
 });
 
-app.get('/api/pronos/:id', (req, res) => {
-    const matchId = '' + req.params.id;
-    const match = cache.get(matchId);
-    res.send(match);
+app.get('/api/matches', (req, res) => {
+    res.send(Array.from(matches.values()));
 });
-
-app.post('/api/pronos', function (req, res) {
-    req.body.id = '' + req.body.id;
-    cache.set(req.body.id, req.body, duration);
-    res.send(req.body);
-})
 
 app.listen(5000, () => {
     console.log("Running on port 5000.");
 });
+
 // Export the Express API
 module.exports = app;
